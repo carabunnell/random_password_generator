92e057322eef0d52d2707c4470d888c610bb8e44
Cara Bunnell <carabunnell@gmail.com>
2020-01-18 18:30:50 -0800

    adding user input verification

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //global variable definitions 
-var lengthOfPassword //this is taken from user input to determine total password length
+ //this is taken from user input to determine total password length
 // these variables are for maybe calculating the array amount....we'll see
             // var divide1
             // var divide2
             // var divide3
             // var divide4 
 // new vars
-var totalDivide // technially not needed, this var is used to add the other divides up
-var newLength // this is the final array amount each used
+// var totalDivide // technially not needed, this var is used to add the other divides up
+// var newLength // this is the final array amount each used
 
 // syncing to html id's of lenght: #password-length-total
 //would you like lowercase #lowercase-answer
@@ -15,10 +15,10 @@ var newLength // this is the final array amount each used
 
 var lengthOfPassword = document.querySelector("#password-length-total");
 // these variables are taken from user input and will be used to determine how much of each array to include in final password
-var input1lower = document.querySelector("#lowercase-answer");
-var input2upper = document.querySelector("#uppercase-answer");
-var input3number = document.querySelector("#number-answer");
-var input4special = document.querySelector("#specialchar-answer"); 
+// var input1lower = document.querySelector("#lowercase-answer");
+// var input2upper = document.querySelector("#uppercase-answer");
+// var input3number = document.querySelector("#number-answer");
+// var input4special = document.querySelector("#specialchar-answer"); 
 var finalProduct = document.querySelector("#user-password-final");
 var submitting = document.querySelector("#sign-up");
 
@@ -37,28 +37,39 @@ var totalOutputSpecial = "";
 submitting.addEventListener("click", function(event) {
     event.preventDefault();
 
+var userPassLength = document.getElementById("password-length-total").value;
 var userLowerAnswer = document.getElementById("lowercase-answer").value;
+var userUpperAnswer = document.getElementById("uppercase-answer").value;
+var userNumberAnswer = document.getElementById("number-answer").value;
+var userSpecialAnswer = document.getElementById("specialchar-answer").value;
+// console.log(userPassLength);
+// console.log(input1lower + input2upper + input3number + input4special);
+var totalDivideHere = JSON.parse(userLowerAnswer) + JSON.parse(userUpperAnswer) + JSON.parse(userNumberAnswer) + JSON.parse(userSpecialAnswer);
+console.log("password length: " + userPassLength);
+console.log("sum of input: " + totalDivideHere);
+
+if (userPassLength == totalDivideHere) {
+    
+
 for (i = 0; i < userLowerAnswer; i++) {
     var output = array1lower[Math.floor(Math.random() * 25)];
     totalOutput = totalOutput + output;
 }
 console.log(userLowerAnswer + " lower Case: " + totalOutput);
 
-var userUpperAnswer = document.getElementById("uppercase-answer").value;
+
 for (i = 0; i < userUpperAnswer; i++) {
     var output2 = array2upper[Math.floor(Math.random() * 25)];
     totalOutputUpper = totalOutputUpper + output2;
 }
 console.log(userUpperAnswer + " upper Case: " + totalOutputUpper);
 
-var userNumberAnswer = document.getElementById("number-answer").value;
 for (i = 0; i < userNumberAnswer; i++) {
     var output3 = array3number[Math.floor(Math.random() * 8)];
     totalOutputNum = totalOutputNum + output3;
 }
 console.log(userNumberAnswer + " numbers: " + totalOutputNum);
 
-var userSpecialAnswer = document.getElementById("specialchar-answer").value;
 for (i = 0; i < userSpecialAnswer; i++) {
     var output4 = array4special[Math.floor(Math.random() * 7)];
     totalOutputSpecial = totalOutputSpecial + output4;
@@ -76,6 +87,20 @@ console.log("Temporary Password: " + temporaryPassword);
 var newArray = temporaryPassword.split("");
 // console.log(newArray);
 
+shuffle(newArray);
+console.log("Scrambled Password: " + newArray.join(""));
+var finalPassword = newArray.join("");
+finalProduct.textContent = finalPassword;
+
+}
+
+else {
+    alert("You suck, make your numbers add up, moron")
+}
+
+});
+
+
 function shuffle(arra1) {
     var ctr = arra1.length, temp, index;
 
@@ -92,14 +117,6 @@ function shuffle(arra1) {
     }
     return arra1;
 }
-
-shuffle(newArray);
-console.log("Scrambled Password: " + newArray.join(""));
-var finalPassword = newArray.join("");
-finalProduct.textContent = finalPassword;
-
-});
-
 // will need to do a split me thinks
 // // varification of unput: 
 // if (input1lower + input2upper + input3number + input4special !== lenthOfPassword) {
